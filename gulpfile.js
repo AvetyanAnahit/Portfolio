16aa2e550615028f99117da9fe185b036333bd01
unknown <you@example.com>
2018-03-11 15:47:45 +0300

    fontss

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+// svgSprite = require('gulp-svg-sprite');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
+// gulp.src('path/to/assets/*.svg')
+//     .pipe(svgSprite('./icons/archive'/* ... Insert your configuration here ... */ ))
+//     .pipe(gulp.dest('out'));
 
 gulp.task('sass', function () {
  return gulp.src('scss/main.scss')
    .pipe(sass({includePaths: ['_/scss/']}).on('error',  sass.logError))
    .pipe(gulp.dest('./css'));
 });
+
 gulp.task('sass:watch', function () {
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('scss/*.scss', gulp.series('sass'));
 });
